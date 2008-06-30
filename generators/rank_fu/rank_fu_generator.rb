e0a081652f7d063529fbf3e89da20ea05fe33ee5
Simon Parsons <simon@intridea.com>
2008-06-30 00:46:25 -0400

    Can now create *either* the roles table or add the role column to another model

--- generators/rank_fu/rank_fu_generator.rb
+++ generators/rank_fu/rank_fu_generator.rb
@@ -1,13 +1,12 @@
-class RoleAdderGenerator < Rails::Generator::NamedBase  
+class RankFuGenerator < Rails::Generator::NamedBase  
   
   attr_accessor :migration_name
     
-  def manifest 
-    @migration_name = "AddRolesTo#{class_name}"   
-    
-    record do |m|               
-      m.migration_template 'lib/roles_migration.rb',"db/migrate", :migration_file_name => "create_roles"
-      m.migration_template 'lib/model_migration.rb',"db/migrate", :migration_file_name => @migration_name.underscore   
+  def manifest         
+    @migration_name = "AddRolesTo#{class_name}"                
+    record do |m|          
+      m.migration_template 'roles_migration.rb',"db/migrate", :migration_file_name => "create_roles" if table_name == "roles"                                                                                                                           
+      m.migration_template 'model_migration.rb',"db/migrate", :migration_file_name => @migration_name.underscore unless table_name == "roles"  
     end
   end 
   
